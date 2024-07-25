a40c4429ab0775e282652ddcbb60ecaa3cc0ea21
whong <weitian.hong@3ds.com>
2024-07-25 23:11:39 +0800

    合并图片识别文件

--- vision.py
+++ vision.py
@@ -1,16 +1,45 @@
 import cv2 as cv
 import time
+import torch
+from torchvision import transforms as T
+from PIL import Image
 
 import config
-from recognition import Recognition
 
 class Vision:
 
     # properties
-    recog = Recognition()
+    model = None
 
     def __init__(self):
-        self.recog = Recognition()
+        # load the trained model
+        self.model = torch.hub.load('baudm/parseq', 'parseq', pretrained=True).eval()
+        self._preprocess = T.Compose([
+            T.Resize((32, 128), T.InterpolationMode.BICUBIC),
+            T.ToTensor(),
+            T.Normalize(0.5, 0.5)
+        ])
+
+    def convertToText(self, image):
+        if image is None:
+            return ''
+        image = Image.fromarray(image).convert('RGB')
+        image = self._preprocess(image).unsqueeze(0)
+        # Greedy decoding
+        pred = self.model(image).softmax(-1)
+        label, _ = self.model.tokenizer.decode(pred)
+        raw_label, raw_confidence = self.model.tokenizer.decode(pred, raw=True)
+        # Format confidence values
+        max_len = len(label[0]) + 1
+        conf = list(map('{:0.1f}'.format, raw_confidence[0][:max_len].tolist()))
+        raw_output = label[0], [raw_label[0][:max_len], conf]
+        if config.DEBUG:
+            print('raw output: ', raw_output)
+        # ('100', [['1', '0', '0', '[E]'], ['0.3', '0.4', '0.3', '0.4']])
+        if float(conf[0]) > 0.8:
+            return label[0]
+        else:
+            return ''
 
     def get_ability_key(self, screenshot_np):
         # 技能按键区域
@@ -19,7 +48,7 @@ class Vision:
         if ability_key_image.size == 0:
             print("技能按键区域图像为空，可能是配置的区域超出了原图的范围。")
             return ''
-        key_text = self.recog.convertToText(ability_key_image)
+        key_text = self.convertToText(ability_key_image)
         if config.DEBUG:
             cv.imwrite('images/Key_{}_{}.jpg'.format(key_text, time.time()), ability_key_image)
         
@@ -32,7 +61,6 @@ class Vision:
         else:
             return ''
 
-
     def get_ability_cooldown(self, screenshot_np):
         # 技能冷却时间区域
         ability_cooldown_image = screenshot_np[config.ABILITY_COOLDOWN_Y:config.ABILITY_COOLDOWN_Y+config.ABILITY_COOLDOWN_H,
@@ -40,7 +68,7 @@ class Vision:
         if ability_cooldown_image.size == 0:
             print("技能冷却时间区域图像为空，可能是配置的区域超出了原图的范围。")
             return
-        cooldown_text = self.recog.convertToText(ability_cooldown_image)
+        cooldown_text = self.convertToText(ability_cooldown_image)
         if config.DEBUG:
             cv.imwrite('images/Cooldown_{}_{}.jpg'.format(cooldown_text, time.time()), ability_cooldown_image)
 
