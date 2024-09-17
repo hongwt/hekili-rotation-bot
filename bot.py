205ae998dfbb79f42624d0ab82a95aaf88f113eb
whong <weitian.hong@3ds.com>
2024-09-17 08:51:41 +0800

    图像二极化处理，避免误识别

--- bot.py
+++ bot.py
@@ -66,9 +66,9 @@ class WowBot:
             if screenshot is None:
                 continue
 
-            # screenshot = screenshot.convert('L')
             # 使用point方法应用这个函数
-            # screenshot = screenshot.point(self.to_white_or_black)
+            screenshot = screenshot.convert('L')
+            screenshot = screenshot.point(self.to_white_or_black)
             
             screenshot_np = np.array(screenshot)
             loop_time = time.time()
